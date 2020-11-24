db4ff838ac68d18b2c412718a1e00be90a2a36c1
Noureddine Haouari <nhaouari@usthb.dz>
2020-11-25 00:03:55 +0100

    v0.0.1

--- README.md
+++ README.md
@@ -1,45 +1,18 @@
-## Obsidian Sample Plugin
+# Youglsih Plugin
+![](./1.gif)
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is plugin uses [youglish widget](https://youglish.com/developer.jsp). Use YouTube to improve your pronunciation. With more than 50M tracks, YouGlish gives you fast, unbiased answers about how words is spoken by real people and in context. 
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+## Install
+- If you have previous versions of plugin - remove them
+- You need Obsidian v0.9.12+
+- Get [Latest release]()
+- Extract files and place them to your vault's plugins folder: `<vault>/.obsidian/plugins/`
+- Reload Obsidian
+- If prompted about Safe Mode, you can disable safe mode and enable the plugin. Otherwise head to Settings, third-party plugins, make sure safe mode is off and enable the plugin from there.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+ 
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## How to use
 
-
-### Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments.
-- Publish the release.
-
-### Adding your plugin to the community plugin list
-
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-### How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-### Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+- Select the phrase you want to look for, and click on youglish plugin icon. 
--- main.ts
+++ main.ts
@@ -1,66 +1,58 @@
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import YG from "widget"
-export default class MyPlugin extends Plugin {
-	widget:any;
-	views:number;
-	curTrack: number; 
-	totalTracks:number;
-	language:string;
-	accent:string;
+export default class YouglishPlugin extends Plugin {
+	widget: any;
+	views = 0;
+	curTrack = 0;
+	totalTracks = 0;
+	language = "english";
 
-	onYouglishAPIReady(str:string){
+	onYouglishAPIReady(str: string) {
 		this.widget = new YG.Widget("widget-1", {
-		  width: 640,
-		  components:255, 
-		  events: {
-			'onFetchDone': this.onFetchDone,
-			'onVideoChange': this.onVideoChange,
-			'onCaptionConsumed': this.onCaptionConsumed
-		  }          
+			width: 640,
+			components: 255,
+			events: {
+				'onFetchDone': this.onFetchDone,
+				'onVideoChange': this.onVideoChange,
+				'onCaptionConsumed': this.onCaptionConsumed
+			}
 		});
 		// 4. process the query
-		this.widget.fetch(str,this.language,this.accent);
+		this.widget.fetch(str, this.language);
 	}
 
-	 // 5. The API will call this method when the search is done
-	  onFetchDone(event:any){
-		if (event.totalResult === 0)   alert("No result found");
-		else this.totalTracks = event.totalResult; 
-	  }
-		 
-	  // 6. The API will call this method when switching to a new video. 
-	   onVideoChange(event:any){
+	// 5. The API will call this method when the search is done
+	onFetchDone(event: any) {
+		if (event.totalResult === 0) alert("No result found");
+		else this.totalTracks = event.totalResult;
+	}
+
+	// 6. The API will call this method when switching to a new video. 
+	onVideoChange(event: any) {
 		this.curTrack = event.trackNumber;
 		this.views = 0;
-	  }
-		 
-	  // 7. The API will call this method when a caption is consumed. 
-	   onCaptionConsumed(event:any){
+	}
+
+	// 7. The API will call this method when a caption is consumed. 
+	onCaptionConsumed(event: any) {
 		if (++this.views < 3)
-		  this.widget.replay();
-		else 
-		  if (this.curTrack < this.totalTracks)  
-			this.widget.next();
-	  }
+			this.widget.replay();
+		else
+			if (this.curTrack < this.totalTracks)
+				this.widget.next();
+	}
 
 	onload() {
-		console.log('loading plugin');	
-		
-		 this.views = 0;
-		 this.curTrack = 0;
-		 this.totalTracks = 0;
-	   
-		
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
+		console.log('loading plugin');
+		this.addRibbonIcon('languages', 'Youglish Plugin', () => {
+			new SampleModal(this.app).open();
+			let leaf = this.app.workspace.activeLeaf;
+			this.onYouglishAPIReady(leaf.view.currentMode.cmEditor.getSelection());
 		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
 
 		this.addCommand({
 			id: 'open-youglish-modal',
-			name: 'Open youglish Modal',
+			name: 'Say It!',
 			// callback: () => {
 			// 	console.log('Simple Callback');
 			// },
@@ -69,7 +61,7 @@ export default class MyPlugin extends Plugin {
 				if (leaf) {
 					if (!checking) {
 						new SampleModal(this.app).open();
-						this.onYouglishAPIReady("courage");
+						this.onYouglishAPIReady(leaf.view.currentMode.cmEditor.getSelection());
 						console.log(this)
 					}
 					return true;
@@ -79,16 +71,6 @@ export default class MyPlugin extends Plugin {
 		});
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerEvent(this.app.on('codemirror', (cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		}));
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -99,39 +81,52 @@ export default class MyPlugin extends Plugin {
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
-		
 	}
 
 	onOpen() {
-		let {contentEl} = this;
-		this.contentEl.innerHTML='<div id="widget-1"></div>'
-		console.log(contentEl)
-		
-		
+		let { contentEl } = this;
+		this.contentEl.innerHTML = '<div id="widget-1"></div>'
 	}
 
 	onClose() {
-		let {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
+	plugin: YouglishPlugin
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for youglish plugin.'});
-		
+		containerEl.createEl('h2', { text: 'Settings for Youglish plugin.' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange((value) => {
-					console.log('Secret: ' + value);
-				}));
+			.setName('Language')
+			.setDesc('The language of youtube videos')
+			.addDropdown((cb) => {
+				cb.addOption("arabic", "Arabic");
+				cb.addOption("english", "English");
+				cb.addOption("french", "French");
+				cb.addOption("spanish", "Spanish");
+				cb.addOption("italian", "Italian");
+				cb.addOption("portuguese", "Portuguese");
+				cb.addOption("german", "German");
+				cb.addOption("korean", "Korean");
+				cb.addOption("turkish", "Turkish");
+				cb.addOption("russian", "Russian");
+				cb.addOption("chinese", "Chinese");
+				cb.addOption("dutch", "dutch");
+				cb.addOption("greek", "greek");
+				cb.addOption("hebrew", "hebrew");
+				cb.addOption("japanese", "japanese");
+				cb.addOption("polish", "polish");
+				cb.addOption("sign language", "Sign language");
+				cb.setValue(this.plugin.language);
+				cb.onChange(value => this.plugin.language = value);
+			})
 
 	}
 }
