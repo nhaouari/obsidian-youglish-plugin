0779c75da7bc504db113f624d152c47e75cce4af
Noureddine Haouari <nhaouari@usthb.dz>
2020-11-24 21:45:35 +0100

    first working example

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import YG from "widget"
 export default class MyPlugin extends Plugin {
+	widget:any;
+	views:number;
+	curTrack: number; 
+	totalTracks:number;
+
+	onYouglishAPIReady(){
+		this.widget = new YG.Widget("widget-1", {
+		  width: 640,
+		  components:255	, //search box & caption 
+		  events: {
+			'onFetchDone': this.onFetchDone,
+			'onVideoChange': this.onVideoChange,
+			'onCaptionConsumed': this.onCaptionConsumed
+		  }          
+		});
+		// 4. process the query
+		this.widget.fetch("courage","english","us");
+	}
+
+	 // 5. The API will call this method when the search is done
+	  onFetchDone(event:any){
+		if (event.totalResult === 0)   alert("No result found");
+		else this.totalTracks = event.totalResult; 
+	  }
+		 
+	  // 6. The API will call this method when switching to a new video. 
+	   onVideoChange(event:any){
+		this.curTrack = event.trackNumber;
+		this.views = 0;
+	  }
+		 
+	  // 7. The API will call this method when a caption is consumed. 
+	   onCaptionConsumed(event:any){
+		if (++this.views < 3)
+		  this.widget.replay();
+		else 
+		  if (this.curTrack < this.totalTracks)  
+			this.widget.next();
+	  }
+
 	onload() {
-		console.log('loading plugin');
+		console.log('loading plugin');	
+		
+		 this.views = 0;
+		 this.curTrack = 0;
+		 this.totalTracks = 0;
+	   
+		
 
 		this.addRibbonIcon('dice', 'Sample Plugin', () => {
 			new Notice('This is a notice!');
@@ -11,8 +57,8 @@ export default class MyPlugin extends Plugin {
 		this.addStatusBarItem().setText('Status Bar Text');
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
+			id: 'open-youglish-modal',
+			name: 'Open youglish Modal',
 			// callback: () => {
 			// 	console.log('Simple Callback');
 			// },
@@ -21,6 +67,8 @@ export default class MyPlugin extends Plugin {
 				if (leaf) {
 					if (!checking) {
 						new SampleModal(this.app).open();
+						this.onYouglishAPIReady();
+						console.log(this)
 					}
 					return true;
 				}
@@ -49,11 +97,17 @@ export default class MyPlugin extends Plugin {
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
+		
 	}
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		this.contentEl.innerHTML='<div id="widget-1"></div>'
+		
+
+		console.log(contentEl)
+		
+		
 	}
 
 	onClose() {
@@ -68,8 +122,8 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
+		containerEl.createEl('h2', {text: 'Settings for youglish plugin.'});
+		
 		new Setting(containerEl)
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
